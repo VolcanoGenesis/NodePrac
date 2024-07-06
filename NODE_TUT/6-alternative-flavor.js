c2fe2ff25a17ac4fec99cc8f11c177ecc65e8a43
Volcanogenesis <aditya.ranjan1712@gmail.com>
2024-07-06 12:00:13 +0530

    New folder for node tuts

---
+++ NODE_TUT/6-alternative-flavor.js
@@ -0,0 +1,7 @@
+module.exports.items = ['item1', 'item2']
+const person= {
+    name:'bob',
+}
+
+module.exports.singlePerson = person
+// these two syntaxes are equivalent to the one used in files 4,5.
